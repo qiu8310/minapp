3a4bc71a71ccd3a5be8cf405f247bd71d7ee35a0
Mora <qiuzhongleiabc@126.com>
2018-02-17 19:53:24 +0800

    feat<generator>: 添加 slot 的定义

--- packages/generator/src/cli/cmd-tpl.ts
+++ packages/generator/src/cli/cmd-tpl.ts
@@ -1,5 +1,4 @@
-import {rootNode} from '../generator/layout/'
-import {warn} from '../generator/base'
+import {warn, rootNode, Type, Component, ComponentAttr} from '../generator/'
 import * as async from 'async'
 import * as fs from 'fs-extra'
 import {COLLECT} from './collect'
@@ -26,6 +25,16 @@ export default async function(res: any, nodeIterator: any) {
       if (err) return console.log(err)
       if (nodes.length !== EXPECT) return
       let cps = COLLECT.TPL.COMPONENTS
+
+      if (!cps.find(cp => cp.name === 'slot')) {
+        let cp = new Component('slot', 'https://mp.weixin.qq.com/debug/wxadoc/dev/framework/custom-component/wxml-wxss.html#%E7%BB%84%E4%BB%B6wxml%E7%9A%84slot')
+        cp.desc = ['用于承载组件使用者提供的wxml结构']
+        cp.attrs = [new ComponentAttr('name', new Type('string'), ['区分不同的组件的 wxml'])]
+        cps.push(cp)
+      } else {
+        warn('slot 已经在官方文档中集成了，无需自定义')
+      }
+
       fs.writeFileSync(OUTPUT.DETAIL_COMPONENTS, JSON.stringify(cps.map(c => c.toJSON())))
       fs.writeFileSync(OUTPUT.COMPONENTS, JSON.stringify(cps.map(c => c.toJSON(['examples'])), null, 2))
     }
