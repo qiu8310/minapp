25e77bd728de27e923e0427512d6da88c4ad6420
Mora <qiuzhongleiabc@126.com>
2018-05-03 15:05:05 +0800

    fix: 修复 minapp-wx 依赖问题

--- packages/minapp-wx/src/wxp.ts
+++ packages/minapp-wx/src/wxp.ts
@@ -3,7 +3,13 @@
  Author Mora <qiuzhongleiabc@126.com> (https://github.com/qiu8310)
 *******************************************************************/
 
-import './wx'
+/// <reference path="../typing/app.d.ts" />
+/// <reference path="../typing/behavior.d.ts" />
+/// <reference path="../typing/component.d.ts" />
+/// <reference path="../typing/page.d.ts" />
+/// <reference path="../typing/wx.d.ts" />
+/// <reference path="../typing/wxp.d.ts" />
+
 import './polyfill'
 
 const PROMISABLE: {FUNCS: string[], KLASS: {[name: string]: string[]}} = {FUNCS: [], KLASS: {}}
